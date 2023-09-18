8912fd910baa400651a0ca33108223e0f6692af4
u1050411@gmail.com <@Pjnuka2001>
2023-09-18 23:08:32 +0200

    Screen , buttons, y checkbox funcionan

--- app/src/main/java/com/trueta/gtei/GteiActivity.kt
+++ app/src/main/java/com/trueta/gtei/GteiActivity.kt
@@ -1,24 +1,16 @@
 package com.trueta.gtei
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.trueta.gtei.ui.theme.GteiTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
 
 class GteiActivity : ComponentActivity() {
     val viewModel: ScreensViewModel by viewModels()
@@ -29,48 +21,20 @@ class GteiActivity : ComponentActivity() {
             val selectedScreen by viewModel.selectedScreen.collectAsState()
             val message by viewModel.message.collectAsState()
 
-            GteiTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Bottom
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(MaterialTheme.colorScheme.primary)
-                            .padding(5.dp)
-                            .weight(.095f),
-                        contentAlignment = Alignment.BottomCenter,
-                    ) {
-                        MessageDisplay(message)
-                    }
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(MaterialTheme.colorScheme.background)
-                            .weight(.005f),
-                        contentAlignment = Alignment.BottomCenter,
-                    ) {}
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(.9f)
-                            .background(Color.White)
-                            .padding(
-                                start = 5.dp,
-                                end = 5.dp,
-                                top = 1.dp,
-                                bottom = 5.dp
-                            ), // padding right and left, top and bottom
-                        contentAlignment = Alignment.TopCenter
-                    ) {
-                        selectedScreen?.let {
-                            TryDisplay(it, viewModel)  // Pass viewModel as an argument
-                        }
-                    }
-                }
-            }
+            val showScreenTry = remember { mutableStateOf(false) }
+
+            // Cambia el estado después de 3 segundos
+            Handler(Looper.getMainLooper()).postDelayed({
+                showScreenTry.value = true
+            }, 3000)
+
+            ScreensGtei(showScreenTry, selectedScreen, message, viewModel)
         }
     }
 }
+
+
+
+
+
+
--- app/src/main/java/com/trueta/gtei/ScreenCompose.kt
+++ app/src/main/java/com/trueta/gtei/ScreenCompose.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,19 +23,148 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.trueta.gtei.ui.theme.GteiTheme
 
+@Composable
+fun ScreensGtei(showScreenTry: MutableState<Boolean>, selectedScreen: Screen?, message: String, viewModel: ScreensViewModel) {
+   if (showScreenTry.value) {
+       ScreenTry(selectedScreen, message, viewModel)
+    } else {
+       ScreenStart()
+    }
+}
+
+@Composable
+fun ScreenStart() {
+    val padding = 32.dp
+    GteiTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
+        ) {
+            // Primera columna (Textos)
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(0.8f)
+                    .padding(top = padding),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top
+            ) {
+                // Título y subtitulo
+                Text(
+                    text = "GUIA DE TRACTAMENT EMPÍRIC DE LES INFECCIONS",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    ),
+                    modifier = Modifier.padding(start = padding)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "\nHOSPITAL UNIVERSITARI DE GIRONA DOCTOR JOSEP TRUETA. EQUIP PROA",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Light,
+                        color = Color.White
+                    ),
+                    modifier = Modifier.padding(start = padding)
+                )
+            }
+
+            // Segunda columna (Imagen en el fondo)
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(0.2f)
+                    .align(Alignment.BottomCenter) // Esto coloca la columna en la parte inferior
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight(0.5f)
+                        .align(Alignment.Start) // Esto coloca la columna en la parte inferior
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo_trueta_generalitat),
+                        contentDescription = "Logo Generalitat",
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(start = padding)
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight(0.5f)
+                        .align(Alignment.End) // Esto coloca la columna en la parte inferior
+                ) {}
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenTry(selectedScreen: Screen?, message: String, viewModel: ScreensViewModel) {
+    GteiTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(5.dp)
+                    .weight(.095f),
+                contentAlignment = Alignment.BottomCenter,
+            ) {
+                MessageDisplay(message)
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.background)
+                    .weight(.005f),
+                contentAlignment = Alignment.BottomCenter,
+            ) {}
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(.9f)
+                    .background(Color.White)
+                    .padding(
+                        start = 5.dp,
+                        end = 5.dp,
+                        top = 1.dp,
+                        bottom = 5.dp
+                    ), // padding right and left, top and bottom
+                contentAlignment = Alignment.TopCenter
+            ) {
+                selectedScreen?.let {
+                    TryDisplay(it, viewModel)  // Pass viewModel as an argument
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun TryDisplay(screen: Screen, viewModel: ScreensViewModel) {
